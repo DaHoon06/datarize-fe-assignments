ae8219c890ec035805eae5be7a27f95af8fa86d4
dahoon06 <dahoon226@naver.com>
2024-11-09 19:44:17 +0900

    chore: jest 설정

---
+++ apps/frontend/jest.config.mjs
@@ -0,0 +1,11 @@
+// jest.config.mjs
+export default {
+  verbose: true,
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
